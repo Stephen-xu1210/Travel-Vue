e6c849a50faf123c2c34417672d628e578a60d7a
stephen-xu <435471845@qq.com>
2018-12-03 14:01:57 +0800

    'performance'

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,15 @@
 <template>
 	<ul class="list">
-		<li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+		<li class="item"
+			v-for="item of letters" 
+			:key="item"
+			:ref="item"
+			@touchstart="handleTouchStart"
+			@touchmove="handleTouchMove"
+			@touchend="handleTouchEnd"
+			@click="handleLetterClick">
+			{{item}}
+		</li>
 	</ul>
 </template>
 <script>
@@ -8,6 +17,51 @@ export default{
 	name:'CityAlphabet',
 	props:{
 		cities:Object
+	},
+	data(){
+		return{
+			touchStatus:false,
+			startY: 0,
+			timer: null
+		}
+	},
+	updated(){
+		this.startY = this.$refs['A'][0].offsetTop
+	},
+	methods:{
+		handleLetterClick(e){
+			this.$emit('change',e.target.innerText)
+		},
+		handleTouchStart(){
+			this.touchStatus = true
+		},
+		handleTouchMove(e){
+			// 此处的对于timer的操作 是函数节流的方式   提升网页性能
+			if(this.touchStatus){
+				if(this.timer){
+					clearTimeout(this.timer)
+				}
+				this.timer = setTimeout(()=>{
+					const touchY = e.touches[0].clientY - 74
+				const index = Math.floor((touchY - this.startY)/20)
+				if(index>=0 && index<this.letters.length){
+					this.$emit('change',this.letters[index])
+				}
+			},16)	
+			}
+		},
+		handleTouchEnd(){
+			this.touchStatus = false
+		}
+	},
+	computed:{
+		letters(){
+			const letters = []
+			for(let i in this.cities){
+				letters.push(i)
+			}
+			return letters
+		}
 	}
 }
 </script>
